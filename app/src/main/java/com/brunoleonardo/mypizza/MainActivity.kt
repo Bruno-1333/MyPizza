38262c8193a81add7c2da651bcad6ee23dfb1f25
Bruno-1333 <2295132@cegepoutaouais.qc.ca>
2023-06-13 11:30:09 -0400

    version fini

--- app/src/main/java/com/brunoleonardo/mypizza/MainActivity.kt
+++ app/src/main/java/com/brunoleonardo/mypizza/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.brunoleonardo.mypizza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    // Action sur le button
+    fun goToSecondActivity(view: View) {
+        // definition de l'intent pour lancer l'activité 2
+        val intent = Intent(this, MainActivity2::class.java)
+        // methode pour lancer l'activité 2
+        startActivity(intent)
     }
 
 
--- app/src/main/java/com/brunoleonardo/mypizza/MainActivity2.kt
+++ app/src/main/java/com/brunoleonardo/mypizza/MainActivity2.kt
@@ -1,11 +1,21 @@
 package com.brunoleonardo.mypizza
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    // Action sur le button
+    fun goToPremiereActivity(view: View) {
+        // definition de l'intent pour lancer l'activité 2
+        val intent = Intent(this, MainActivity::class.java)
+        // methode pour lancer l'activité 2
+        startActivity(intent)
     }
 }
